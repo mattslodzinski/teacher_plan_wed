dd6d78d306d0827ac4d3930e5e86d00c68aa05f6
Raghu Betina <raghu@firstdraft.com>
2018-02-28 15:01:13 +0000

    Direct association between resource_neededs and action_steps

--- app/models/action_step.rb
+++ app/models/action_step.rb
@@ -1,6 +1,9 @@
 class ActionStep < ApplicationRecord
   # Direct associations
 
+  belongs_to :resource,
+             :class_name => "ResourceNeeded"
+
   has_many   :users,
              :foreign_key => "action_id"
 
--- app/models/resource_needed.rb
+++ app/models/resource_needed.rb
@@ -1,6 +1,9 @@
 class ResourceNeeded < ApplicationRecord
   # Direct associations
 
+  has_many   :action_steps,
+             :foreign_key => "resource_id"
+
   # Indirect associations
 
   # Validations
