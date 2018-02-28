2a938b771589cffd63c058fea37ae4ed2f2dd1ab
Raghu Betina <raghu@firstdraft.com>
2018-02-28 15:01:41 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "action_steps#index"
   # Routes for the Measures_of_success resource:
   # CREATE
